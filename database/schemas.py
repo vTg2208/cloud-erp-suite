da024e4fa7ff6ddce1b385c0698ed1109b258b3a
Nithish S <150901506+NithishProgrammer@users.noreply.github.com>
2026-05-04 16:20:35 +0530

    Create schemas.py

---
+++ database/schemas.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import date
+from typing import Optional
+
+class EmployeeCreate(BaseModel):
+    eid: str
+    employee_name: str
+    department: str
+    salary: str  
+    doj: date    # Pydantic automatically converts "YYYY-MM-DD" strings to date objects
+
+class EmployeeResponse(EmployeeCreate):
+    class Config:
+        from_attributes = True
